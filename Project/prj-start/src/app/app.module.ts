3f245896e9eb3ec379fbf5d5cb5d0b6e20baef82
ajaykc7 <azay.kc137@gmail.com>
2018-06-20 23:31:22 -0700

    Navigation bar placed (styling and functionality left)

--- Project/prj-start/src/app/app.module.ts
+++ Project/prj-start/src/app/app.module.ts
@@ -5,11 +5,21 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent} from './header/header.component';
+import { RecipesComponent } from './header/recipes/recipes.component';
+import { RecipeListComponent } from './header/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './header/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './header/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './header/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
